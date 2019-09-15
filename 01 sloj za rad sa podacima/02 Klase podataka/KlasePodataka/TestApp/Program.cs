b7f320dedb84e925d184127bb60d938e79d48765
‘brkicz’ <zoricabrkic96@gmail.com>
2019-09-15 02:04:29 +0200

    Dodate klase podataka

---
+++ 01 sloj za rad sa podacima/02 Klase podataka/KlasePodataka/TestApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KlasePodataka;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProizvodjacDB proizvodjac = new ProizvodjacDB();
+           
+            //var data = proizvodjac.DajSveProizvodjace().Tables[0].Rows[0].ItemArray[2].ToString();
+
+            ModelDB model = new ModelDB();
+            var data = model.DajSveModele().Tables[0].Rows[0].ItemArray[2].ToString();    
+        }
+    }
+}
